09e565ab452b1f2d2f3c1e260d65fa711c185a58
Deividas Ragauskas <deividasrg@gmail.com>
2024-04-04 20:41:56 +0300

    .

---
+++ SvarosNamai/Models/ProductBundle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SvarosNamai.Service.ProductAPI.Models
+{
+    public class ProductBundle
+    {
+        public Bundle BundleId { get; set; }
+        public IEnumerable<Product> Products { get; set;}
+    }
+}
