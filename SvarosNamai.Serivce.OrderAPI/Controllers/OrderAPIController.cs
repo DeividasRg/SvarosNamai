2eeae3d0eed0c33e1f15a6539635f3a07ed61ec2
Deividas Ragauskas <deividasrg@gmail.com>
2024-04-06 15:42:30 +0300

    CreateOrder endpoint

--- SvarosNamai.Serivce.OrderAPI/Controllers/OrderAPIController.cs
+++ SvarosNamai.Serivce.OrderAPI/Controllers/OrderAPIController.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SvarosNamai.Serivce.OrderAPI.Models;
 using SvarosNamai.Serivce.OrderAPI.Service.IService;
 using SvarosNamai.Service.OrderAPI.Data;
 using SvarosNamai.Service.OrderAPI.Models.Dtos;
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 
 namespace SvarosNamai.Serivce.OrderAPI.Controllers
@@ -31,7 +33,38 @@ namespace SvarosNamai.Serivce.OrderAPI.Controllers
         [HttpPost("CreateOrder")]
         public async Task<ResponseDto> CreateOrder(OrderDto order, int bundleId)
         {
+            try
+            {
+                BundleDto bundleFromDb = await _productService.GetBundle(bundleId);
+                if (bundleFromDb != null)
+                {
+                    Order orderToDb = _mapper.Map<Order>(order);
+                    orderToDb.CreationDate = DateTime.Now;
+                    await _db.Orders.AddAsync(orderToDb);
+                    
 
+
+                    foreach (var product in bundleFromDb.Products)
+                    {
+                        OrderLine orderline = new OrderLine();
+                        orderline.Order = orderToDb;
+                        orderline.ProductName = product.Name;
+                        await _db.OrderLines.AddAsync(orderline);
+                        await _db.SaveChangesAsync();
+                    }
+                    
+
+
+                    _response.Message = "OrderCreated";
+
+                }
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
         }
     }
 }
---
+++ SvarosNamai.Serivce.OrderAPI/Migrations/20240406123258_add_db4.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SvarosNamai.Serivce.OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_db4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderLines_Orders_OrderId",
+                table: "OrderLines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderLines_OrderId",
+                table: "OrderLines");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderId",
+                table: "OrderLines",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderLines_Orders_OrderId",
+                table: "OrderLines",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
