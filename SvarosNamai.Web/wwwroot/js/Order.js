380a97c06a8d69acff6fd445c723572c25a94098
Deividas Ragauskas <Dragauskas@barbora.lt>
2024-04-16 15:18:37 +0300

    Need to fix order view

--- SvarosNamai.Web/wwwroot/js/Order.js
+++ SvarosNamai.Web/wwwroot/js/Order.js
@@ -6,12 +6,28 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: "/order/getall" },
+        "ajax": { url: "/order/GetAll" },
         "columns": [
-            { data: 'orderId', width: "5%"},
-            { data: 'email', width: "25%"},
-
+            { data: 'orderId', width: "5%" },
+            {
+                data: function (row) {
+                    return row.name + ' ' + row.lastName;
+                },
+                width: "10%"
+            },
+            {
+                data: function (row) {
+                    return row.street + ' ' + row.houseNo + (row.houseLetter != null ? row.houseLetter : "") + (row.apartmentNo != null ? " - " + row.apartmentNo : "") + ', ' + row.city;
+                },
+                width: "20%"
+            },
+            { data: 'hour', width: "5%" },
+            {
+                data: function (row) {
+                    return SD.GetStatusDescription(row.Status);
+                },
+                width: "10%"
+            }
         ]
-
-    })
+    });
 }
